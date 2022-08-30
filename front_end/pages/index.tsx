b5625d9ecd91327581b09db5cba4909c8b687eaa
Sam Sun <samsun@SamdeMBP-2.lan>
2022-08-30 18:52:15 -0400

    change setting and finish first page

---
+++ front_end/pages/index.tsx
@@ -0,0 +1,61 @@
+import { Grid, Button, Typography } from "@mui/material";
+import { createTheme } from "@mui/material";
+import Router from "next/router";
+require("typeface-eb-garamond");
+import "@fontsource/montserrat";
+import { GiWindHole } from "react-icons/gi";
+
+const theme = createTheme({
+  components: {
+    MuiTypography: {
+      defaultProps: {
+        variantMapping: {
+          h1: "h2",
+          h2: "h2",
+          h3: "h2",
+          h4: "h2",
+          h5: "h2",
+          h6: "h2",
+          subtitle1: "h2",
+          subtitle2: "h2",
+          body1: "span",
+          body2: "span",
+        },
+      },
+    },
+  },
+});
+
+function HomePage(props: {}) {
+  return (
+    <div style={{ height: "100vh", background: "black" }}>
+      <div className="waterPrint">
+        <span>By 20-</span>
+        <GiWindHole />
+        <span>-40 Studio</span>
+      </div>
+      <div className="heading">
+        <div className="headingFont">Welcome To The World Of</div>
+        <div className="headingFont"> Yiran Sun !</div>
+        <Button
+          variant="outlined"
+          style={{
+            color: "white",
+            borderColor: "white",
+            borderRadius: 0,
+            width: "35%",
+            fontFamily: "Montserrat",
+            fontSize: "13px",
+            fontWeight: 600,
+            height: "40px",
+            top: "17vh",
+          }}
+        >
+          Get Started
+        </Button>
+      </div>
+    </div>
+  );
+}
+
+export default HomePage;
--- my-app/pages/index.tsx
+++
@@ -1,8 +0,0 @@
-import { Grid, Button} from "@mui/material"
-import router from 'next/router'
-
-function HomePage(props:{}) {
-  return
-}
-
-export default HomePage;
